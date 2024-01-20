51b32a5f89e06a521a05b025635bb93c47b407c0
Songül Çelik <songulcelik3537@gmail.com>
2024-01-20 22:54:44 +0300

    v-1

--- src/actions/product-actions.js
+++ src/actions/product-actions.js
@@ -1,42 +1,101 @@
 "use server";
-
 import { config } from "@/utils/config";
+import { convertFormDataToJson, getYupErrors } from "@/utils/form-validation";
 import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import * as Yup from "yup";
-
 const FormSchema = Yup.object({
-	title: Yup.string().required("Required"),
-	description: Yup.string().required("Required"),
-	category: Yup.string().required("Required"),
-	price: Yup.number().typeError("Invalid number").required("Required"),
-	image: Yup.string().required("Required"),
+    id: Yup.string().required(),
+    title: Yup.string().required("Required"),
+    description: Yup.string().required("Required"),
+    category: Yup.string().required("Required"),
+    price: Yup.number().typeError("Invalid number").required("Required"),
+    image: Yup.string().required("Required"),
 });
-
-export const createProductAction = async (formData) => {
-	// Form data turunde  gelen form bilgilerini javascript object ine cevirdik
-	const fields = Object.fromEntries(formData.entries());
-
-	try {
-		FormSchema.validateSync(fields, { abortEarly: false });
-
-		const resp = await fetch(`${config.apiURL}/products`, {
-			method: "post",
-			body: JSON.stringify(fields),
-			headers: {
-				"Content-Type": "application/json",
-			},
-		});
-
-		if (!resp.ok) {
-		}
-	} catch (err) {
-		if (err instanceof Yup.ValidationError) {
-			err.inner.forEach((error) => console.log(error.path, error.message));
-		}
-	}
-
-	revalidatePath("/products");
-	revalidatePath("/dashboard/products");
-	redirect("/dashboard/products");
+const CreateSchema = FormSchema.omit(["id"]);
+export const createProductAction = async (prevState, formData) => {
+    // Form data turunde  gelen form bilgilerini javascript object ine cevirdik
+    const fields = convertFormDataToJson(formData);
+    try {
+        CreateSchema.validateSync(fields, { abortEarly: false });
+        const resp = await fetch(`${config.apiURL}/products`, {
+            method: "post",
+            body: JSON.stringify(fields),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        if (!resp.ok) {
+            const data = await resp.json();
+            throw new Error(data.message);
+        }
+    } catch (err) {
+        console.log(err);
+        if (err instanceof Yup.ValidationError) {
+            return getYupErrors(err.inner);
+        }
+        return {
+            message: "Something went wrong",
+            errors: {
+                commonError: err.message,
+            },
+        };
+    }
+    revalidatePath("/products");
+    revalidatePath("/dashboard/products");
+    redirect("/dashboard/products");
+};
+export const updateProductAction = async (prevState, formData) => {
+    const fields = convertFormDataToJson(formData);
+    try {
+        FormSchema.validateSync(fields, { abortEarly: false });
+        const resp = await fetch(`${config.apiURL}/products/${fields.id}`, {
+            method: "put",
+            body: JSON.stringify(fields),
+            headers: {
+                "Content-Type": "application/json",
+            },
+        });
+        if (!resp.ok) {
+            const data = await resp.json();
+            throw new Error(data.message);
+        }
+    } catch (error) {
+        if (error instanceof Yup.ValidationError) {
+            return getYupErrors(error.inner);
+        }
+        return {
+            message: "Something went wrong",
+            errors: {
+                commonError: err.message,
+            },
+        };
+    }
+    revalidatePath("/products");
+    revalidatePath(`/products/${fields.id}`);
+    revalidatePath("/dashboard/products");
+    revalidatePath(`/dashboard/products/${fields.id}`);
+    redirect("/dashboard/products");
+};
+export const deleteProductAction = async (id) => {
+    try {
+        if (!id) throw new Error("id is missing");
+        const res = await fetch(`${config.apiURL}/products/${id}`, {
+            method: "delete",
+        });
+        if (!res.ok) {
+            const data = await res.json();
+            throw new Error(data.message);
+        }
+    } catch (error) {
+        return {
+            message: "Something went wrong",
+            errors: {
+                commonError: error.message,
+            },
+        };
+    }
+    revalidatePath("/products");
+    revalidatePath("/dashboard/products");
+    redirect("/dashboard/products");
 };
--- src/components/dashboard/products/btn-delete-product.jsx
+++ src/components/dashboard/products/btn-delete-product.jsx
@@ -1,20 +1,28 @@
 "use client";
+import { deleteProductAction } from "@/actions/product-actions";
 import React from "react";
 import { Button } from "react-bootstrap";
 import { FaRegTrashAlt } from "react-icons/fa";
-
 const BtnDeleteProduct = ({ id }) => {
-	const handleClick = () => {
-		const resp = confirm("Are you sure to delete?");
-
-		if (!resp) return;
-	};
-
-	return (
-		<Button variant="link" onClick={handleClick}>
-			<FaRegTrashAlt />
-		</Button>
-	);
+    const handleClick = async () => {
+        const answer = confirm("Are you sure to delete?");
+        if (!answer) return;
+        try{
+            const res = await deleteProductAction(id);
+            if(res.errors.commonError){
+                alert(res.errors.commonError)
+            }
+        }
+        catch(error){
+            console.log(error)
+            
+        }
+        
+    };
+    return (
+        <Button variant="link" onClick={handleClick}>
+            <FaRegTrashAlt />
+        </Button>
+    );
 };
-
 export default BtnDeleteProduct;
